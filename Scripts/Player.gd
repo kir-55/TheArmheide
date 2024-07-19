2509a9ceb8a66b6067e6a3c0da46aa79748a9a34
drompus-local <drompmaz2008@gmail.com>
2024-07-19 17:06:57 +0200

    Previous Main House menu is now adjustable for other buildings and player now stops when opening the menu

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,7 +9,7 @@ var direction = 0
 var jump_pressed = false
 var run_endless = false
 
-var destinationX: float = 0.0
+var destination_x: float = 0.0
 
 @export var is_in_air = true
 
@@ -24,9 +24,9 @@ func _physics_process(delta):
 	if direction != 0:
 		get_node("Sprite2D").scale.x = direction * 0.25
 	if !run_endless:
-		if destinationX - global_position.x > 5:
+		if destination_x - global_position.x > 5:
 			direction = 1
-		elif destinationX - global_position.x < -5:
+		elif destination_x - global_position.x < -5:
 			direction = -1
 		else:
 			direction = 0
@@ -62,14 +62,20 @@ func _process(delta):
 	elif !is_on_floor() or get_floor_normal().angle() > deg_to_rad(100) or get_floor_normal().angle() < deg_to_rad(-80):
 		animation.play("PlayerFly")
 
-func _input(event):
+func _unhandled_input(event):
+	print("cos")
 	if event is InputEventMouseButton:
-		if event.is_action_pressed("shoot"):
+		if event.is_action_pressed("click"):
 			if event.double_click:
 				run_endless = true
 			else:
-				destinationX = get_global_mouse_position().x
+				destination_x = get_global_mouse_position().x
 				run_endless = false
+
+func stop():
+	direction = 0
+	run_endless = false
+	destination_x = position.x
 
 func jump():
 	motion.y = -JUMP_STRENGTH
