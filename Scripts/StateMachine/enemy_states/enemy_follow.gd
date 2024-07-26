ee70f017bdcf726cad12a92cd9148e1169f2cb76
drompus-local <drompmaz2008@gmail.com>
2024-07-26 16:51:09 +0200

    Updated enemy functionality (now the enemy has a timer to get closest enemy in range)

--- Scripts/StateMachine/enemy_states/enemy_follow.gd
+++ Scripts/StateMachine/enemy_states/enemy_follow.gd
@@ -4,16 +4,13 @@ extends State
 @export var enemy : CharacterBody2D
 @export var move_speed := 120.0
 
-var target : CharacterBody2D
-
 func Enter():
-	target = get_tree().get_first_node_in_group("willagers")
 	print(self.name)
 
 
 func Physics_Update(delta: float):
-	if enemy.is_on_floor():
-		var direction = target.global_position - enemy.global_position
+	if enemy.is_on_floor() and enemy.current_closest_target:
+		var direction = enemy.current_closest_target.global_position - enemy.global_position
 		
 		if direction.length() < 500:
 			enemy.velocity = direction.normalized() * move_speed
